4b6b34848043a4127ad0f790df759e4293aefca8
FelipePaterni <felipe.chaves2@etec.sp.gov.br>
2024-05-30 19:55:52 -0300

    Exemplo de stylesheet
    Aqui mostra testes e como usar o stylesheet

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View , ScrollView} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Template que sera usado a outros projetos</Text>
-      <StatusBar style="auto" />
+      <ScrollView>{/*Parte de rolagem de tela*/}
+        <View style={ [styles.Card, styles.red]}> {/*Parte de rolagem de tela*/}
+
+        <Text style={styles.textoBase}>Testando a aplicação </Text>
+        <Text style={styles.textoInterno}>Testando texto interno :D</Text>
+
+        </View>
+
+        <View style={ [styles.Card, styles.blue]}> {/*Parte de rolagem de tela*/}
+
+        <Text style={styles.textoBase}>Testando a aplicação </Text>
+        <Text style={styles.textoInterno}>Testando texto interno :D</Text>
+
+        </View>
+
+       
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +29,39 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 40,
+
+  },
+
+  blue:{
+    backgroundColor: '#836fff',
+
+  },
+  
+  red:{
+    backgroundColor: '#f08080',
+
+  },
+
+  Card:{
+    marginTop:10,
+    height:100,
+    borderRadius: 3,
+    width:300,
+  },
+
+  textoBase:{
+    color:'#000000',
+    fontWeight:800,
+
+  },
+
+  textoInterno:{
+    color:'red',
+    fontWeight:400,
+    
   },
 });
