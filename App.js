5244ac9f6215693671614c9a3eb98346c3177ece
FelipePaterni <felipe.chaves2@etec.sp.gov.br>
2024-05-30 19:53:40 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Template que sera usado a outros projetos</Text>
       <StatusBar style="auto" />
     </View>
   );
